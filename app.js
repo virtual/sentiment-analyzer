ef16254386d4fa46e9be1e799bd0a2fcd02fc0ed
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-06-06 06:49:40 -0600

    try adding node env for replit preview

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const sa = require('./index.js')
-// app.get("/public", (req, res) => {
-//   res.redirect("/");
-// });
+
+require('dotenv').config();
+
+/* passport has strategies which are functions that prove that a user trying to hit your server has permission */
+if (process.env.NODE_ENV === 'production') { 
+  app.use(express.static("./public/build"));
+} else {
+  app.use(express.static("public"));  
+}
+
+app.get("/", (req, res) => {
+  // res.redirect("/3000");
+  console.log('connected to /')
+});
 // Needed in order to read axios req data
 app.use(express.json())
 
