ae6ad5a609f3998025b4aa8add975f762cd172f5
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-06-07 04:47:14 -0600

    lift state, add dynamic coloring and styles

--- public/src/App.js
+++ public/src/App.js
@@ -1,19 +1,69 @@
+import React, {Component} from 'react';
 import "./App.css";
 import FormLyrics from './FormLyrics';
-// import { text } from "express";
 
-function App() {
-return (
-	<div className="App">
-	<header className="App-header">
-  <p>Sentiment Analyzer</p>
-	</header>
-  <main>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wordsInput: '',
+      sentiment: '0',
+      bgHue: 'hsl(161, 17%, 88%)'
+    };
 
-    <FormLyrics />
-  </main>
-	</div>
-);
+    this.onWordsChange = this.onWordsChange.bind(this);
+    this.updateSA = this.updateSA.bind(this);
+    this.createHSLValue = this.createHSLValue.bind(this);
+  }
+  
+  onWordsChange(event) {
+    this.setState({wordsInput: event.target.value});
+  }
+
+  createHSLValue(saValue) {
+    const val = parseFloat(saValue);
+    const valp = Math.abs(val/3);
+    let intensity = val !== 0 ? Math.abs(1/(Math.log(Math.abs(val/3)))) : .17;
+    if (!(isFinite(intensity))) { intensity = 1; }
+    console.log(val, valp, intensity)
+    var pct = Math.abs(intensity*100).toFixed(1) + "%";
+    if (val > 0) {
+      return `hsl(212, ${pct}, 82%)`
+    } else if (val < 0) {
+      return `hsl(0, ${pct}, 90%)`
+    } else {
+      return `hsl(161, ${pct}, 88%)`
+    }
+  }
+
+  updateSA(saValue) {
+    let newHSL = this.createHSLValue(saValue);
+
+    this.setState({
+      sentiment: saValue,
+      bgHue: newHSL
+    });
+  }
+
+  render() {
+    var saPct = (this.state.sentiment*100).toFixed(1) + "%";
+
+    return (
+      <div className="App" style={{backgroundColor: this.state.bgHue}}>
+        <header className="App-header">
+          <h1>Sentiment Analyzer</h1>
+        </header>
+        <main>
+          <FormLyrics 
+            wordsInput={this.state.wordsInput}
+            sentiment={saPct}
+            onWordsChange={this.onWordsChange} 
+            updateSA={this.updateSA}
+            />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- public/src/FormLyrics.js
+++ public/src/FormLyrics.js
@@ -1,69 +1,51 @@
 import React, {Component} from 'react';
+import "./FormLyrics.css";
 const axios = require('axios')
 class FormLyrics extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      wordsInput: '',
-      sentiment: '0'
-    };
 
-    this.handleWordsChange = this.handleWordsChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.updateSA = this.updateSA.bind(this);
-  }
-
-  handleWordsChange(event) {
-    this.setState({wordsInput: event.target.value});
-  }
-
-  updateSA(saValue) {
-    this.setState({sentiment: saValue});
   }
 
   handleSubmit(event) {
     var saValue = '';
-    try {
-      // axios.post('/post', {
-      //   body: this.state.value
-      // })
-      axios({
-        method: 'post',
-        url: '/post',
-        data: {
-          lyrics: this.state.value
-        }
-      })
-      .then((response) => {
-        console.log(response.data);
-        saValue = response.data;
-        this.updateSA(saValue)
-      }, (error) => {
-        console.log(error);
-      });
-    } catch (error) {
-      console.error(error)
+    if (this.props.wordsInput !== '') {
+      try {
+        axios({
+          method: 'post',
+          url: '/post',
+          data: {
+            lyrics: this.props.wordsInput
+          }
+        })
+        .then((response) => {
+          console.log(response.data);
+          saValue = response.data;
+          this.props.updateSA(saValue)
+        }, (error) => {
+          console.log(error);
+        });
+      } catch (error) {
+        console.error(error)
+      }
     }
-    // alert('A name was submitted: ' + );
-    // console.log(data)
-    // this.setState({sentiment: 'arf'+saValue});
     event.preventDefault();
   }
 
   render() {
     return (
-      <div>
-        Sentiment: {this.state.sentiment}
+      <div className="wordsForm">
+        <h2>Sentiment: {this.props.sentiment}</h2>
         <form onSubmit={this.handleSubmit}  action="../../post" method="post">
           <label>
             <small>Words:</small>
             <br/>
-            <textarea rows="10" type="text" value={this.state.wordsInput} onChange={this.handleWordsChange} />
+            <textarea rows="10" type="text" value={this.props.wordsInput} onChange={this.props.onWordsChange} />
           </label>
           <br/>
           <input type="submit" value="Submit" />
         </form>
-        <p>Positive numbers are happy; negative are unhappy</p>
       </div>
     );
   }
